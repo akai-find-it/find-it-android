5d20649c0b575abf20d09bb90fbc75d879d748ca
luncenok <idziejczak3@gmail.com>
2022-11-05 10:56:35 +0100

    add camera support

--- app/src/main/java/pl/org/akai/hackathon/ui/item/ItemFragment.kt
+++ app/src/main/java/pl/org/akai/hackathon/ui/item/ItemFragment.kt
@@ -1,6 +1,11 @@
 package pl.org.akai.hackathon.ui.item
 
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -28,10 +33,33 @@ class ItemFragment : BaseFragment<ItemFragmentBinding>(ItemFragmentBinding::infl
 			vm.loadData()
 		}
 
+		b.imageView2.setOnClickListener {
+			dispatchTakePictureIntent()
+		}
+
 		vm.data.observe(viewLifecycleOwner) {
 			it ?: return@observe
-			b.dateText.text = "{cmd-calendar} Znaleziono dnia ${it.foundAt.format(DateTimeFormatter.ISO_DATE)}"
+			b.dateText.text =
+				"{cmd-calendar} Znaleziono dnia ${it.foundAt.format(DateTimeFormatter.ISO_DATE)}"
 			b.founderText.text = "{cmd-account} ${it.founder.firstName} ${it.founder.lastName}"
+		}
+	}
+
+	private fun dispatchTakePictureIntent() {
+		val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+		try {
+			startActivityForResult(takePictureIntent, 1)
+		} catch (e: ActivityNotFoundException) {
+			// display error state to the user
+		}
+
+	}
+
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		if (requestCode == 1 && resultCode == RESULT_OK) {
+			val imageBitmap = data?.extras?.get("data") as Bitmap
+			b.imageView2.setImageBitmap(imageBitmap)
 		}
 	}
 }
